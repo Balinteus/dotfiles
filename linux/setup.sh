fc7dd26582bc9ea2263ddee924bf3c169c9b16e9
Bálint Gonda <balinteus@gmail.com>
2024-06-30 14:08:17 +0200

    .dotfiles v2.0

---
+++ linux/setup.sh
@@ -0,0 +1,7 @@
+PREFIX_ADDITION="\e[0;32;1m[+]"
+PREFIX_DELETION="\e[0;31;1m[-]"
+PREFIX_LINKING="\e[1;34;1m[<->]"
+
+echo -e "${PREFIX_ADDITION} Performing a full system update"
+echo -e "${PREFIX_ADDITION} Installing packages"
+echo -e "${PREFIX_LINKING} Symlinking configs in .config"
