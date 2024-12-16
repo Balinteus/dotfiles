08484ae8b59124e2f924945c0a54cbefcb1be322
Bálint Gonda <balinteus@gmail.com>
2024-12-16 23:39:45 +0100

    Add option to use NVIM plugins without VC tracking

---
+++ linux/.config/nvim/lua/plugins/settings/temporary.lua
@@ -0,0 +1,8 @@
+-- Put those plugins here, that you don't want to keep under version control
+return {
+  -- {
+  --    "user/example-plugin.nvim",
+  --    dependencies = { "user/plugin_dep1.nvim", "user/plugin_dep2.nvim" },
+  --    opts = {}
+  -- },
+}
